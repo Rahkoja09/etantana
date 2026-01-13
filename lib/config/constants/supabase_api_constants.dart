272e65be9ab978a38dff9862fdd8b8c9bd1c8ae0
Rahkoja09 <ramanamahefakoja.mg@gmail.com>
2026-01-13 14:22:04 +0300

    remove some api key supabase

--- lib/config/constants/supabase_api_constants.dart
+++ lib/config/constants/supabase_api_constants.dart
@@ -1,5 +1,5 @@
 class SupabaseApiConstants {
-  static String apiUrl = "https://cmpbikydmwpfabreimai.supabase.co";
+  static String apiUrl = "https://cmpbikydmwpfabreimai.null.co";
   static String apiKey =
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNtcGJpa3lkbXdwZmFicmVpbWFpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDYyNTc3MTMsImV4cCI6MjA2MTgzMzcxM30.KS5DGPOQAW0Wig2UEbyDDhS4TvwF3iuKG9LLUaLMzfA";
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNtcGJpa3lkbXdwZmFicmVpbWFpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDYyNTc3MTMsImV4cCI6MjA2MTgzMzcxM30.null null null null";
 }
