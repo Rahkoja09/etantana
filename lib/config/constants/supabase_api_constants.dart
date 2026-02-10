6645be592d45e290463b7b72e20bd42a96ef96da
Rahkoja09 <ramanamahefakoja.mg@gmail.com>
2026-02-10 21:41:44 +0300

    delete all secret token in public : no important data

--- lib/config/constants/supabase_api_constants.dart
+++ lib/config/constants/supabase_api_constants.dart
@@ -1,5 +1,5 @@
 class SupabaseApiConstants {
-  static String apiUrl = "https://unrhzngoirzyszxzybok.supabase.co";
+  static String apiUrl 
   static String apiKey =
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVucmh6bmdvaXJ6eXN6eHp5Ym9rIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQxOTY3NDgsImV4cCI6MjA3OTc3Mjc0OH0.5Bx4glXZKGncOC-et8ne9skZWKnVvCdgQwtaW1glRrA";
+      "";
 }
